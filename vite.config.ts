2730bab3ada47f03c3e05bed944e0077a44db4d4
Tohid Dayhami <tohid.dayhami@gmail.com>
2026-06-05 22:18:50 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig(({ mode }) => {
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
+        'process.env.VITE_FIREBASE_PROXY': JSON.stringify(env.VITE_FIREBASE_PROXY || '')
       },
       resolve: {
         alias: {
